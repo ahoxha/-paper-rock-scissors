b07564dab391f12b4c7c1d03dd68049b9c575904
Armend Hoxha <ahoxha4biz@gmail.com>
2019-11-04 13:13:13 +0100

    Delete unnecessary comment.

--- src/main/java/com/armend/game/rules/DecisionRules.java
+++ src/main/java/com/armend/game/rules/DecisionRules.java
@@ -14,8 +14,6 @@ public interface DecisionRules {
 	 * Decides which item is the winner and returns the winning item. If there is a
 	 * tie, it will return null.
 	 * 
-	 * @param item1 The first item.
-	 * @param item2 The second item.
 	 * @return The item that is the winner or null if there is a tie between them.
 	 */
 	Item whoIsTheWinner(Item item1, Item item2);
