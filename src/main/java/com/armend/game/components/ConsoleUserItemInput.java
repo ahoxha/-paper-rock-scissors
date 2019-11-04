c4c8387b0bfb5bb5134d9bad4c71eefacfaea9dd
Armend Hoxha <ahoxha4biz@gmail.com>
2019-11-04 13:42:27 +0100

    Refactor code.

--- src/main/java/com/armend/game/components/ConsoleUserItemInput.java
+++ src/main/java/com/armend/game/components/ConsoleUserItemInput.java
@@ -20,22 +20,31 @@ public class ConsoleUserItemInput implements ItemInput {
 		Item item;
 		int tries = 3;
 		do {
-			System.out.println("Type S for Scissors, R for Rock, P for Paper:");
-			String input;
-			try {
-				input = reader.readLine();
-			} catch (IOException e) {
-				input = "";
-			}
+			String input = askUserForInput();
 			item = Item.of(input);
-			if (item == null) {
+			if (isWrongInput(item)) {
 				System.err.println("Wrong input: " + input);
 			}
 			tries--;
-		} while (item == null && tries > 0);
-		if (item == null) {
-			System.out.print("Failed to get a valid input after 3 consequetive tries.");
+		} while (isWrongInput(item) && tries > 0);
+		if (isWrongInput(item)) {
+			System.out.print(String.format("Failed to get a valid input after %s consequetive tries.", tries));
 		}
 		return item;
+	}
+
+	private boolean isWrongInput(Item item) {
+		return item == null;
+	}
+
+	private String askUserForInput() {
+		System.out.println("Type S for Scissors, R for Rock, P for Paper:");
+		String input;
+		try {
+			input = reader.readLine();
+		} catch (IOException e) {
+			input = "";
+		}
+		return input;
 	}
 }
